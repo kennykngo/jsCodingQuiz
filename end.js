4ba57c3f1497744f319241e5f783c3ceccc1c99a
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-04-26 18:47:40 -0700

    added end/ highscore pages

--- end.js
+++ end.js
@@ -3,6 +3,11 @@ const saveScoreBtn = document.querySelector("#saveScoreBtn");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.querySelector("#finalScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 10;
+// get what's in localStorage or return an empty array
+console.log(highScores);
 finalScore.innerText = mostRecentScore;
 
 initials.addEventListener("keyup", () => {
@@ -13,4 +18,25 @@ initials.addEventListener("keyup", () => {
 saveHighScore = (e) => {
   console.log("clicked the save button");
   e.preventDefault();
+
+  const score = {
+    // score: Math.floor(Math.random() * 100),
+    score: mostRecentScore,
+    name: initials.value,
+  };
+  highScores.push(score);
+
+  highScores.sort((a, b) => b.score - a.score);
+
+  highScores.splice(10);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+
+  window.location.assign("./highScores.html");
+
+  // sortingHigh = (a, b) => {
+  //   a.score > b.score ? a.score - b.score : b.score - a.score;
+  // };
+
+  console.log(highScores);
 };
