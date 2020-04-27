46d665330ea9cb961f90176b745bf6795e3c47e7
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-04-26 19:08:15 -0700

    page styles update, linked view highscores

--- highScores.js
+++ highScores.js
@@ -3,6 +3,16 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores
   .map((score) => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    return `<li class="high-score text-center"><u>${score.name} - ${score.score}</u></li>`;
   })
   .join("");
+
+// highScoresList.innerHTML = highScores
+//   .map((score) => {
+//     for (var i = 0; i < highScoresList.length; i++) {
+//       return `<li class="high-score text-center"><u>${highScoresList[i + 1]} +${
+//         score.name
+//       } - ${score.score}</u></li>`;
+//     }
+//   })
+//   .join("");
